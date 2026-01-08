cc85dc4c800e659152e8b03371634a2b98cd162e
Shivarajkushals <shivaraj.mb@kushals.com>
2026-01-08 19:34:11 +0530

    Update Cetralized_returns.py

--- Cetralized_returns.py
+++ Cetralized_returns.py
@@ -1160,7 +1160,8 @@ elif st.session_state.page == "upload":
 
     # Disable RTV and RTO pages
     if selected_page in ["RTV page", "RTO page"]:
-        st.warning("🚫 This page is temporarily disabled. Please contact admin.")
+        st.warning("🚫 This page is temporarily disabled. Please contact admin.\n"
+                  "Click on the Sidebar on top right corner to access other pages.")
         st.stop()
     
     # Use selected_page variable instead of page variable
